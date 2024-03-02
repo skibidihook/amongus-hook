ae415d59f3456baa176ce370de6c288cf71d702a
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2024-03-02 10:13:10 +1000

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -6,7 +6,7 @@ local str = "https://raw.githubusercontent.com/mainstreamed/amongus-hook/main/";
 if (placeid == 13800717766) then
     loadstring(game:HttpGet(str.."fallensurvival/main.lua", true))()
 elseif (placeid == 13253735473) then
-    loadstring(game:HttpGet(str.."phantomforces/main.lua", true))()
+    loadstring(game:HttpGet(str.."tridentsurvival/main.lua", true))()
 elseif (placeid == 292439477) then
-    loadstring(game:HttpGet(str.."tridentsurvival/main.lua"))()
+    loadstring(game:HttpGet(str.."phantomforces/main.lua"))()
 end
