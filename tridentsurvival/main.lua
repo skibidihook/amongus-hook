f3b29183f65fba41515129f020b792bb74effcb0
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2024-11-05 10:55:26 +1000

    Update main.lua

--- tridentsurvival/main.lua
+++ tridentsurvival/main.lua
@@ -1,7 +1,5 @@
 -- p100 actor functionality
 
-local run_on_actor = nil
-
 local source = game:HttpGet('https://raw.githubusercontent.com/mainstreamed/amongus-hook/refs/heads/main/tridentsurvival/obfuscated.lua');
 local actor_functionality = run_on_actor and getactors;
 
