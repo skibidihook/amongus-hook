22fa3e2bfc6559c3a392440f885d22b770ceba4e
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2024-11-05 17:28:02 +1000

    Update main.lua

--- tridentsurvival/main.lua
+++ tridentsurvival/main.lua
@@ -1,9 +1,16 @@
--- p100 actor functionality
+local players           = game:GetService('Players');
+local localplayer       = players.LocalPlayer;
+if (not localplayer) then
+      players:GetPropertyChangedSignal('LocalPlayer'):Wait();
+      localplayer = players.LocalPlayer;
+end;
 
 local source = game:HttpGet('https://raw.githubusercontent.com/mainstreamed/amongus-hook/refs/heads/main/tridentsurvival/obfuscated.lua');
-local actor_functionality = run_on_actor and getactors;
 
-if (actor_functionality) then
-      return run_on_actor(getactors()[1], source);
+if (run_on_actor) then
+      local actor = getactors and getactors()[1] or localplayer:FindFirstChildWhichIsA('Actor', true);
+      if (actor) then
+            return run_on_actor(actor, source);
+      end;
 end;
 loadstring(source)();
