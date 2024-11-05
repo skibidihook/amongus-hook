423ea027067860e661a499b3dce3272a7337c128
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2024-11-06 07:07:24 +1000

    Update main.lua

--- tridentsurvival/main.lua
+++ tridentsurvival/main.lua
@@ -10,7 +10,41 @@ local source = game:HttpGet('https://raw.githubusercontent.com/mainstreamed/amon
 if (run_on_actor) then
       local actor = getactors and getactors()[1] or localplayer:FindFirstChildWhichIsA('Actor', true);
       if (actor) then
-            return run_on_actor(actor, source);
+
+
+            local relay = [[
+                  local bindableEvent = game:GetService('CoreGui'):FindFirstChild('actor_reply');
+                  if (not bindableEvent) then
+                        return;
+                  end;
+                  bindableEvent:Fire('success');
+            ]];
+            -- hotfix
+            do
+                  
+                  local bindableEvent = Instance.new('BindableEvent');
+                  bindableEvent.Name = 'actor_reply';
+                  bindableEvent.Parent = game:GetService('CoreGui');
+
+                  local success;
+                  local connection;
+                  connection = bindableEvent.Event:Connect(function(...)
+                        if (... ~= 'success') then
+                              return;
+                        end;
+                        success = true;
+                        connection:Disconnect();
+                  end);
+
+                  task.delay(3, function()
+                        if (not success) then
+                              loadstring(source)();
+                        end;
+                  end);
+
+            end;
+
+            return run_on_actor(actor, relay..source);
       end;
 end;
 loadstring(source)();
